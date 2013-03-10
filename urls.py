6ad4c95f2442a63c666b9f564be0a592359a61eb
RB <rfboyce@gmail.com>
2013-03-09 20:15:22 -0500

    updated html

--- urls.py
+++ urls.py
@@ -2,6 +2,7 @@ from django.conf.urls.defaults import patterns, include, url
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
+from django.views.generic import TemplateView
 admin.autodiscover()
 
 urlpatterns = patterns('',
@@ -14,4 +15,7 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
+    
+    # serve up base page
+    url(r'^base/$', TemplateView.as_view(template_name="base.html") ),
 )
