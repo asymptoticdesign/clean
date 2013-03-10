d3df670dab8e5e3f59930fde51e86c35e6b6f54f
RB <rfboyce@gmail.com>
2013-03-09 21:54:58 -0500

    update to html

--- urls.py
+++ urls.py
@@ -17,5 +17,4 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
     
-    url(r'^index/$', direct_to_template, {'template':'base.html'}),
 )
